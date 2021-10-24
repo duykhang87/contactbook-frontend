82c6382a1c6de1a633ab0bdb12119c56607a6f99
dkhang <khangb1906687@student.ctu.edu.vn>
2021-10-24 19:45:05 +0700

    Tao trang cap nhap lien he

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
-<template> 
-  <div id="app"> 
-    <nav class="navbar navbar-expand navbar-dark bg-dark"> 
-      <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a> 
-      <div class="mr-auto navbar-nav">
-          <li class="nav-item"> 
-            <router-link to="/contactbook" class="nav-link"> 
-              Danh bạ <i class="fas fa-address-book"></i> 
-            </router-link> 
-          </li> 
-      </div> 
-    </nav> 
-    <div class="container mt-3"> 
-      <router-view /> 
-    </div> 
-  </div> 
-</template> 
+<template>
+    <div id="app">
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
+            <div class="mr-auto navbar-nav">
+                <li class="nav-item">
 
-<script> export default { name: "App", };</script>
+                </li>
+            </div>
+        </nav>
+
+        <div class="container mt-3">
+           
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
